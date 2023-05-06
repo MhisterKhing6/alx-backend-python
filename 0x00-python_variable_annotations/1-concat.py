76edebcda597e9993afc940d31444eb6d1da5871
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-06 13:52:31 +0000

    2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Create a funtion concat with proper annotations of parameters
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    concat : Add two valuse a and b
+    Args:
+        a : string to concat to b
+        b : string to concat to a
+    return float
+    """
+    return a + b
