a8da876443ba4cc1687fee59cdd5b788a896483a
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-06 16:42:14 +0000

    3

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -9,7 +9,7 @@ import typing
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
     safe_first_elemnt: Anoting a function with unkown container parameters
-    args: 
+    args:
     lst: the list of sequence to annotate
     """
     if lst:
