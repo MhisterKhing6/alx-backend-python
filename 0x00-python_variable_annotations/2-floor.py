3345ece9022024fa675074c6a49b477091ee73ab
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-06 13:59:11 +0000

    2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Create a funtion floor with proper annotations of parameters
+"""
+import math
+
+
+def floor(a: float) -> int:
+    """
+    floor : returns the floor value of floot b
+    Args:
+        a : value to return the floor
+    return float
+    """
+    return int (math.floor(a))
