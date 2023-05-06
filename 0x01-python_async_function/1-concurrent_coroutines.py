baee8bec727e34012d3d336bc59a0b0bf9f844c9
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-06 06:10:38 +0000

    1 correction

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(number: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     wait_n : runs multiple caro rutines in parallel
     Arguements:
@@ -16,6 +16,6 @@ async def wait_n(number: int, max_delay: int = 10) -> List[float]:
         n : The number of caro rutines
     """
     res = await asyncio.gather(
-        *(wait_random(max_delay) for _ in range(number))
+        *(wait_random(max_delay) for _ in range(n))
         )
     return sorted(res)
