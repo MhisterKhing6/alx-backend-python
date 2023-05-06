0671dc06b575bde2ea206ca329a0913159fbcd1e
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-06 13:46:08 +0000

    1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Create a funtion Add with proper annotations of parameters
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    add : Add two valuse a and b
+    Args:
+        a : value to add
+        b : value to add to a
+    return float
+    """
+    return a + b
