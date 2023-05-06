3f31cec579719a7ec38549380fad6488bf373a2b
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-06 14:07:12 +0000

    3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Create a function that returns the string representation of an object
+"""
+
+
+def to_str(a: float) -> str:
+    """
+    to_str : returns the string representation of
+    Args:
+        a : string to concat to b
+    return string representation of arg a
+    """
+    return str(a)
