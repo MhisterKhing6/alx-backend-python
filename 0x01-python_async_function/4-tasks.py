b301c834080b95fa8959ab9f2c84a6c1cee1b435
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-06 07:13:38 +0000

    last

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+Write a function that run multiiple caro rutines in parallel
+'''
+import asyncio
+from typing import List
+wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    wait_n : runs multiple caro rutines in parallel
+    Arguements:
+        max_delay: The maximum time the funcion shoud sleed
+        n : The number of caro rutines
+    """
+    res = await asyncio.gather(
+        *(wait_random(max_delay) for _ in range(n))
+        )
+    return sorted(res)
