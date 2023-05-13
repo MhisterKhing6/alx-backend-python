5571d495594a37c9c2b14d0741eb030e8d0356bc
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-13 03:38:03 +0000

    try

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Implementin async comprehension"""
+"""Measure the time i takes to perform asyn opereration"""
 import asyncio
 from typing import List
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> int:
+async def measure_runtime() -> float:
     """
     measure the time it takes for an asycnhronus operation to complete
     return list of floats
