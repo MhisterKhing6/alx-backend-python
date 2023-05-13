f1be5c619509939703f6d79fbf5e12c5ebcc088c
Kingsley Botchway <mhisterkhing@localhost.localdomain>
2023-05-13 03:12:40 +0000

    22

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+"""Implementin async comprehension"""
 import asyncio
 from typing import Iterable
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> Iterable[float]:
+    """
+    async_comprehension: genrate results from a genrator function
+    return list of floats
+    """
     result = [i async for i in async_generator()]
     return result
